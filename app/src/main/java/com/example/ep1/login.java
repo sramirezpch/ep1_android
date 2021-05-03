0fa5a16f1ab57a17ce882a2a5871b895a38e54ae
sergio1999ram <sramirezpch@gmail.com>
2021-05-02 22:16:12 -0500

    First commit

---
+++ app/src/main/java/com/example/ep1/login.java
@@ -0,0 +1,42 @@
+package com.example.ep1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class login extends AppCompatActivity implements View.OnClickListener {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button btnIngresar = findViewById(R.id.btnLogin);
+        TextView textRegistrar = findViewById(R.id.textViewSignUp);
+        TextView textForgotPassword = findViewById(R.id.textViewForgotPassword);
+
+        btnIngresar.setOnClickListener(this);
+        textRegistrar.setOnClickListener(this);
+        textForgotPassword.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.btnLogin:
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+                break;
+            case R.id.textViewSignUp:
+                startActivity(new Intent(this, signup.class));
+                break;
+            case R.id.textViewForgotPassword:
+                startActivity(new Intent(this, forgotPassword.class));
+                break;
+        }
+
+    }
+}
